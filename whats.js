3cfd0efb7e972b6bb2b368993970c5a5589e2c0a
victor26sp <82735535+victor26sp@users.noreply.github.com>
2023-09-03 15:21:24 -0300

    adc whats env

---
+++ whats.js
@@ -0,0 +1,27 @@
+document.getElementById('sendAllFavoritesButton').addEventListener('click', function () {
+    enviarTodosOsFavoritosViaWhatsApp();
+});
+
+function enviarTodosOsFavoritosViaWhatsApp() {
+    const numeroDestinatario = '+5517996282802'; // Número de telefone padrão
+    let mensagem = 'Itens Favoritados:\n\n';
+
+    const favoritos = products.filter(product => product.isFavorite);
+
+    if (favoritos.length === 0) {
+        alert("Nenhum item favoritado para enviar via WhatsApp.");
+        return;
+    }
+
+    favoritos.forEach((product, index) => {
+        mensagem += `Item ${index + 1}:\n`;
+        mensagem += `Ref: ${product.ref}\n`;
+        mensagem += `Descrição: ${product.description}\n\n`;
+
+        // Adicione as imagens como links
+        mensagem += `![Imagem${index + 1}](${product.image})\n\n`;
+    });
+
+    const linkWaMe = `https://web.whatsapp.com/send?phone=${numeroDestinatario}&text=${encodeURIComponent(mensagem)}`;
+    window.open(linkWaMe, '_blank');
+}
